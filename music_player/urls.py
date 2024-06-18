63e00891994e56de31599ab6b5b114e8b9913651
AnupamaPradhan01 <anupamakripradhan08@gmail.com>
2024-06-18 15:30:38 +0530

    added our urls to main urls.py

--- music_player/urls.py
+++ music_player/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('music_app/', include('music_app.urls', namespace='music_app')),
 ]
