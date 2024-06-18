5fbe00caa15d45ab53efe3bb92d7eeb17e510d20
AnupamaPradhan01 <anupamakripradhan08@gmail.com>
2024-06-18 09:23:21 +0530

    adding status field for language choices

--- music_app/models.py
+++ music_app/models.py
@@ -5,6 +5,11 @@ from django.utils import timezone
 
 
 class Song(models.Model):
+    # adding a status field for language choice
+    class Languages(models.TextChoices):
+        HINDI = 'H', 'Hindi'
+        ENGLISH = 'E', 'English'
+
     name = models.CharField(max_length=250)
     slug = models.CharField(max_length=250)
     album = models.CharField(max_length=250)
@@ -13,6 +18,8 @@ class Song(models.Model):
     song_img = models.ImageField(upload_to='images/')
     song_file = models.FileField(upload_to='uploads/')
     publish = models.DateTimeField(default=timezone.now)
+    lang = models.CharField(
+        max_length=2, choices=Languages.choices, default=Languages.HINDI)
 
     # defining default sort order(reverse chronological order)from newest to oldest
     class Meta:
