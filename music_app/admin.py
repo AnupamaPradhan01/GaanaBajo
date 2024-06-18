e489bc654c145cf2bb880b5ae7a6e6721a1431b1
AnupamaPradhan01 <anupamakripradhan08@gmail.com>
2024-06-18 11:10:51 +0530

    customize admin site

--- music_app/admin.py
+++ music_app/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Song
 
 # register song models
-admin.site.register(Song)
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'singer', 'publish', 'lang']
