d2e929d510b2ad24e083405e55629dcc8cf29dda
AnupamaPradhan01 <anupamakripradhan08@gmail.com>
2024-06-19 17:39:36 +0530

    recommending song via email

--- music_app/views.py
+++ music_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Song
+from .forms import EmailSongForm
 #  display song list .
 
 
@@ -14,3 +15,20 @@ def song_detail(request, year, song):
     song = get_object_or_404(
         Song,  lang=Song.Languages.HINDI,  publish__year=year, slug=song,)
     return render(request, 'music_app/song/song_detail.html', {'song': song})
+
+# song share via email
+
+
+def song_share(request, song_id):
+    # retrieve post by id
+    song = get_object_or_404(Song, id=song_id, lang=Song.Languages.HINDI)
+    if request.method == 'POST':
+        # form was submitted
+        form = EmailSongForm(request.POST)
+        if form.is_valid():
+            # form fields passed validation
+            cd = form.cleaned_data
+            # .....send email
+    else:
+        form = EmailSongForm()
+    return render(request, 'music_app/song/song_share.html')
